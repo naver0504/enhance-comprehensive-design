11026fcf4e1469745250a1f1be039296c510f9bf
naver0504 <qortndwls1234@gmail.com>
2025-03-27 05:33:47 +0900

    feat: Key String -> Long으로 변경(트랜잭션 ID가 파티션 키)

--- batch-service/src/main/java/com/example/command/kafka/config/KafkaProducerConfig.java
+++ batch-service/src/main/java/com/example/command/kafka/config/KafkaProducerConfig.java
@@ -1,6 +1,9 @@
 package com.example.command.kafka.config;
 
+import com.example.command.batch.publish_event.EventItemRecord;
+import com.fasterxml.jackson.databind.JsonSerializer;
 import lombok.RequiredArgsConstructor;
+import org.apache.kafka.common.serialization.LongSerializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -22,10 +25,10 @@ public class KafkaProducerConfig {
     private final KafkaProperties kafkaProperties;
 
     @Bean
-    public ProducerFactory<String, String> producerFactory() {
+    public ProducerFactory<Long, String> producerFactory() {
         Map<String, Object> producerProperties = new HashMap<>();
         producerProperties.put("bootstrap.servers", kafkaProperties.getBootstrapServers());
-        producerProperties.put("key.serializer", StringSerializer.class);
+        producerProperties.put("key.serializer", LongSerializer.class);
         producerProperties.put("value.serializer", StringSerializer.class);
         producerProperties.put("auto.offset.reset", "earliest");
         producerProperties.put("acks", "all");
@@ -34,7 +37,7 @@ public class KafkaProducerConfig {
     }
 
     @Bean
-    public KafkaTemplate<String, String> kafkaTemplate() {
+    public KafkaTemplate<Long, String> kafkaTemplate() {
         return new KafkaTemplate<>(producerFactory());
     }
 }
