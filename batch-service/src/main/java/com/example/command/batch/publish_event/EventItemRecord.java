c25b4297851f5746ebcc8af3d946e3a9982add09
naver0504 <qortndwls1234@gmail.com>
2025-03-27 06:13:38 +0900

    feat: 직렬화 시 제외

--- batch-service/src/main/java/com/example/command/batch/publish_event/EventItemRecord.java
+++ batch-service/src/main/java/com/example/command/batch/publish_event/EventItemRecord.java
@@ -1,6 +1,9 @@
 package com.example.command.batch.publish_event;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 public interface EventItemRecord {
 
+    @JsonIgnore
     Long getPartitionKey();
 }
