69a1b06c12b3fb9cd81c86aadf86068e95e4bec6
naver0504 <qortndwls1234@gmail.com>
2025-03-27 05:34:01 +0900

    feat: Key String -> Long으로 변경(트랜잭션 ID가 파티션 키)

--- search-service/src/main/java/com/example/query/kafka/KafkaConsumerConfiguration.java
+++ search-service/src/main/java/com/example/query/kafka/KafkaConsumerConfiguration.java
@@ -2,7 +2,8 @@ package com.example.query.kafka;
 
 import lombok.RequiredArgsConstructor;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.kafka.common.serialization.LongDeserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
@@ -24,22 +25,25 @@ public class KafkaConsumerConfiguration {
     private final CustomKafkaProperties kafkaProperties;
 
     @Bean
-    public ConsumerFactory<String, String> consumerFactory() {
+    public ConsumerFactory<Long, String> consumerFactory() {
         Map<String, Object> properties = new HashMap<>();
         properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");  // 가장 처음부터 읽기
         properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaProperties.getBootstrapServers());
         properties.put(ConsumerConfig.GROUP_ID_CONFIG, "query-service");
-        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, LongDeserializer.class);
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         properties.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, 1000);
         properties.put(ConsumerConfig.MAX_POLL_INTERVAL_MS_CONFIG, 10000);
         properties.put(ConsumerConfig.RETRY_BACKOFF_MS_CONFIG, 1000);
+        //session timeout
+        properties.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, 9000);
+        properties.put(ConsumerConfig.HEARTBEAT_INTERVAL_MS_CONFIG, 3000);
         return new DefaultKafkaConsumerFactory<>(properties);
     }
 
     @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
-        ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+    public ConcurrentKafkaListenerContainerFactory<Long, String> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<Long, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
         ContainerProperties containerProperties = factory.getContainerProperties();
         containerProperties.setIdleBetweenPolls(3000);
         factory.setConsumerFactory(consumerFactory());
