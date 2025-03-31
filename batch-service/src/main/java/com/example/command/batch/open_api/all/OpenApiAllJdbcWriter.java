a67953a602ed2b219f2f34bbe80b7b7614fcf870
naver0504 <qortndwls1234@gmail.com>
2025-03-31 22:02:57 +0900

    feat: 중복 검사 후 저장하는 Writer 추가

---
+++ batch-service/src/main/java/com/example/command/batch/open_api/all/OpenApiAllJdbcWriter.java
@@ -0,0 +1,88 @@
+package com.example.command.batch.open_api.all;
+
+
+import com.example.command.batch.open_api.dto.ApartmentDetail;
+import com.example.command.batch.open_api.dto.ApartmentDetailResponseWithGu;
+import com.example.command.domain.dong.Gu;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.item.*;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
+@Slf4j
+@RequiredArgsConstructor
+public class OpenApiAllJdbcWriter implements ItemWriter<ApartmentDetailResponseWithGu> {
+
+    private final InterestDataHolder interestDataHolder;
+    private final OpenApiAllGuDataHolder openApiAllGuDataHolder;
+    private final ExistTransactionChecker existTransactionChecker;
+    private final JdbcTemplate jdbcTemplate;
+
+    private final String INSERT_SQL =
+            "INSERT INTO apartment_transaction (" +
+                    "apartment_name, " +
+                    "build_year, " +
+                    "deal_amount, " +
+                    "deal_year, " +
+                    "deal_month, " +
+                    "deal_day, " +
+                    "area_for_exclusive_use, " +
+                    "jibun, " +
+                    "floor, " +
+                    "deal_date, " +
+                    "dealing_gbn," +
+                    "dong_entity_id," +
+                    "interest_id" +
+                    ") " +
+                    "VALUES" +
+                    "( ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+    @Override
+    public void write(Chunk<? extends ApartmentDetailResponseWithGu> chunk) throws Exception {
+        ApartmentDetailResponseWithGu apartmentDetailResponse = chunk.getItems().get(0);
+
+        Gu gu = apartmentDetailResponse.gu();
+
+        List<ApartmentDetail> items = apartmentDetailResponse.toApartmentDetails()
+                .stream()
+                .filter(apartmentDetail -> openApiAllGuDataHolder.getEntityId(gu ,apartmentDetail.dongName().trim()) != null)
+                .takeWhile(apartmentDetail -> !existTransactionChecker.isExistTransaction(gu, apartmentDetail))
+                .toList();
+
+        jdbcTemplate.batchUpdate(INSERT_SQL, new BatchPreparedStatementSetter() {
+            @Override
+            public void setValues(PreparedStatement ps, int i) throws SQLException {
+                try {
+                    ApartmentDetail apartmentDetail = items.get(i);
+                    ps.setString(1, apartmentDetail.apartmentName() == null ? null : apartmentDetail.apartmentName().trim());
+                    ps.setInt(2, apartmentDetail.buildYear());
+                    ps.setInt(3, apartmentDetail.getDealAmount());
+                    ps.setInt(4, apartmentDetail.dealYear());
+                    ps.setInt(5, apartmentDetail.dealMonth());
+                    ps.setInt(6, apartmentDetail.dealDay());
+                    ps.setDouble(7, apartmentDetail.areaForExclusiveUse());
+                    ps.setString(8, apartmentDetail.jibun().trim());
+                    ps.setInt(9, apartmentDetail.floor());
+                    ps.setDate(10, Date.valueOf(apartmentDetail.getDealDate()));
+                    ps.setString(11, apartmentDetail.dealingGbn() == null ? null : apartmentDetail.dealingGbn().name());
+                    ps.setLong(12, openApiAllGuDataHolder.getEntityId(gu, apartmentDetail.dongName().trim()));
+                    ps.setLong(13, interestDataHolder.getEntityId(apartmentDetail.getDealDate()));
+                } catch (Exception e) {
+                    log.error("apartmentDetail : {}", items.get(i));
+                    throw e;
+                }
+            }
+
+            @Override
+            public int getBatchSize() {
+                return items.size();
+            }
+        });
+    }
+}
