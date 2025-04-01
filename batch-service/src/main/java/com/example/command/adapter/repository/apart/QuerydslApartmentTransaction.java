cb5067250cb52874720b33b8cd1effbb8d253a22
naver0504 <qortndwls1234@gmail.com>
2025-04-02 05:06:08 +0900

    fix: 해당 계약 달의 존재하는 Transaction 가져오는 쿼리 수정

--- batch-service/src/main/java/com/example/command/adapter/repository/apart/QuerydslApartmentTransaction.java
+++ batch-service/src/main/java/com/example/command/adapter/repository/apart/QuerydslApartmentTransaction.java
@@ -1,20 +1,23 @@
 package com.example.command.adapter.repository.apart;
 
-import com.example.command.batch.open_api.dto.AlreadyExistTransactioRecord;
+import com.example.command.batch.open_api.dto.ApartmentDetail;
+import com.example.command.domain.apartment.ApartmentTransaction;
 import com.example.command.domain.dong.Gu;
-import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
-import static com.example.command.batch.open_api.dto.AlreadyExistTransactioRecord.*;
 import static com.example.command.domain.apartment.QApartmentTransaction.apartmentTransaction;
 import static com.example.command.domain.dong.QDongEntity.*;
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.mapping;
 
 @RequiredArgsConstructor
 @Repository
@@ -22,41 +25,23 @@ public class QuerydslApartmentTransaction {
 
     private final JPAQueryFactory queryFactory;
 
-    public Map<Gu, AlreadyExistTransactioRecord> findLastTransactionByDealDate(LocalDate startDate) {
-        List<Long> maxIds = queryFactory.select(apartmentTransaction.id.min())
+    public Map<Gu, Set<ApartmentDetail>> findTransactionByContractDate(LocalDate contractDate) {
+        LocalDate startOfMonth = LocalDate.of(contractDate.getYear(), contractDate.getMonth(), 1);
+        LocalDate endOfMonth = startOfMonth.plusMonths(1);
+
+        List<ApartmentTransaction> apartmentTransactions = queryFactory.select(apartmentTransaction)
                 .from(apartmentTransaction)
-                .innerJoin(dongEntity)
+                .innerJoin(dongEntity).fetchJoin()
                 .on(apartmentTransaction.dongEntity.id.eq(dongEntity.id))
-                .where(apartmentTransaction.dealDate.goe(startDate))
-                .groupBy(dongEntity.gu)
+                .where(
+                        apartmentTransaction.dealDate.goe(startOfMonth),
+                        apartmentTransaction.dealDate.lt(endOfMonth)
+                )
                 .fetch();
 
-        System.out.println("maxIds = " + maxIds);
+        if(apartmentTransactions.isEmpty()) return Collections.emptyMap();
 
-        List<AlreadyExistTransactionRecordWithGu> records = queryFactory.select(Projections.constructor(AlreadyExistTransactionRecordWithGu.class,
-                        dongEntity.gu,
-                        apartmentTransaction.dealDate,
-                        apartmentTransaction.jibun,
-                        apartmentTransaction.apartmentName,
-                        apartmentTransaction.floor,
-                        apartmentTransaction.buildYear,
-                        apartmentTransaction.areaForExclusiveUse,
-                        apartmentTransaction.dealAmount
-                ))
-                .from(apartmentTransaction)
-                .innerJoin(dongEntity)
-                .on(apartmentTransaction.dongEntity.id.eq(dongEntity.id))
-                .where(apartmentTransaction.id.in(maxIds))
-                .fetch();
-        System.out.println("records = " + records);
-        return records
-                .stream()
-                .collect(
-                        Collectors.toMap(
-                                AlreadyExistTransactionRecordWithGu::gu,
-                                AlreadyExistTransactioRecord::new
-                        )
-                );
+        return apartmentTransactions.stream()
+                .collect(groupingBy(ApartmentTransaction::getGu, mapping(ApartmentDetail::new, Collectors.toSet())));
     }
-
 }
--- batch-service/src/main/java/com/example/command/batch/open_api/all/ExistTransactionChecker.java
+++ batch-service/src/main/java/com/example/command/batch/open_api/all/ExistTransactionChecker.java
@@ -1,7 +1,7 @@
 package com.example.command.batch.open_api.all;
 
 import com.example.command.adapter.repository.apart.QuerydslApartmentTransaction;
-import com.example.command.batch.open_api.dto.AlreadyExistTransactioRecord;
+import com.example.command.batch.open_api.PostConstructInitiationBean;
 import com.example.command.batch.open_api.dto.ApartmentDetail;
 import com.example.command.domain.dong.Gu;
 import jakarta.annotation.PostConstruct;
@@ -9,24 +9,27 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 
 import java.time.LocalDate;
+import java.util.Collections;
 import java.util.Map;
+import java.util.Set;
 
 @RequiredArgsConstructor
-public class ExistTransactionChecker {
+public class ExistTransactionChecker implements PostConstructInitiationBean {
 
     private final QuerydslApartmentTransaction querydslApartmentTransaction;
-    private Map<Gu, AlreadyExistTransactioRecord> lastTransactionMap;
+    private Map<Gu, Set<ApartmentDetail>> contractMonthTransactionMap;
 
     @Value("#{jobParameters[contractDate]}")
     private LocalDate contractDate;
 
+    @Override
     @PostConstruct
     public void init() {
-        lastTransactionMap = querydslApartmentTransaction.findLastTransactionByDealDate(LocalDate.of(contractDate.getYear(), contractDate.getMonth(), 1));
+        contractMonthTransactionMap = Collections.unmodifiableMap(querydslApartmentTransaction.findTransactionByContractDate(contractDate));
     }
 
-    public boolean isExistTransaction(Gu gu, ApartmentDetail apartmentDetail) {
-        AlreadyExistTransactioRecord lastTransaction = lastTransactionMap.get(gu);
-        return lastTransaction == null || lastTransaction.equals(apartmentDetail);
+    public boolean isNotExistTransaction(Gu gu, ApartmentDetail apartmentDetail) {
+        if(contractMonthTransactionMap.isEmpty()) return true;
+        return !contractMonthTransactionMap.get(gu).contains(apartmentDetail);
     }
 }
