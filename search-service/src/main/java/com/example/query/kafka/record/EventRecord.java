5573c22bc58582260b0171df0ac219779bb5378f
naver0504 <qortndwls1234@gmail.com>
2025-04-04 20:16:01 +0900

    feat: BulkOps를 위한 toQueryUpdate 추가

--- search-service/src/main/java/com/example/query/kafka/record/EventRecord.java
+++ search-service/src/main/java/com/example/query/kafka/record/EventRecord.java
@@ -1,5 +1,9 @@
 package com.example.query.kafka.record;
 
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.data.util.Pair;
+
 public interface EventRecord {
     Long getPartitionKey();
     Pair<Query, Update> toQueryUpdate();
