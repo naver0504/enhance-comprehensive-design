e01edb8f0ec85c346d6020e96cf5c4deaf9c63ff
naver0504 <qortndwls1234@gmail.com>
2025-04-04 20:15:52 +0900

    feat: BulkOps를 위한 toQueryUpdate 추가

--- search-service/src/main/java/com/example/query/kafka/record/UpdateTransactionRecord.java
+++ search-service/src/main/java/com/example/query/kafka/record/UpdateTransactionRecord.java
@@ -1,5 +1,11 @@
 package com.example.query.kafka.record;
 
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.data.util.Pair;
+
+import static org.springframework.data.mongodb.core.query.Criteria.where;
+
 public record UpdateTransactionRecord(long id, long predictCost, boolean isReliable) implements EventRecord {
     @Override
     public Long getPartitionKey() {
