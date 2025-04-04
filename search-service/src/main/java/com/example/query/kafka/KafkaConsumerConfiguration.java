a6b71174185d03f9f4638434e870ca5c6be0a529
naver0504 <qortndwls1234@gmail.com>
2025-04-04 20:15:18 +0900

    refactor: BatchListener로 변환 및 Kafka를 위한 ForkJoinPool 빈 추가

--- search-service/src/main/java/com/example/query/kafka/KafkaConsumerConfiguration.java
+++ search-service/src/main/java/com/example/query/kafka/KafkaConsumerConfiguration.java
@@ -1,54 +1,77 @@
 package com.example.query.kafka;
 
+import com.example.query.kafka.record.CreateTransactionRecord;
+import com.example.query.kafka.record.EventRecord;
+import com.example.query.kafka.record.UpdateTransactionRecord;
 import lombok.RequiredArgsConstructor;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.LongDeserializer;
-import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.listener.ContainerProperties;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ForkJoinPool;
 
 @EnableKafka
 @RequiredArgsConstructor
 @EnableConfigurationProperties(CustomKafkaProperties.class)
 @Profile("!test")
+@Configuration
 public class KafkaConsumerConfiguration {
 
     private final CustomKafkaProperties kafkaProperties;
 
-    @Bean
-    public ConsumerFactory<Long, String> consumerFactory() {
-        Map<String, Object> properties = new HashMap<>();
-        properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");  // 가장 처음부터 읽기
-        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaProperties.getBootstrapServers());
-        properties.put(ConsumerConfig.GROUP_ID_CONFIG, "query-service");
-        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, LongDeserializer.class);
-        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        properties.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, 1000);
-        properties.put(ConsumerConfig.MAX_POLL_INTERVAL_MS_CONFIG, 10000);
-        properties.put(ConsumerConfig.RETRY_BACKOFF_MS_CONFIG, 1000);
-        //session timeout
-        properties.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, 9000);
-        properties.put(ConsumerConfig.HEARTBEAT_INTERVAL_MS_CONFIG, 3000);
-        return new DefaultKafkaConsumerFactory<>(properties);
+    @Bean(name = "kafkaForkJoinPool")
+    public ForkJoinPool kafkaForkJoinPool() {
+        return new ForkJoinPool(7);
     }
 
-    @Bean
-    public ConcurrentKafkaListenerContainerFactory<Long, String> kafkaListenerContainerFactory() {
-        ConcurrentKafkaListenerContainerFactory<Long, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+    @Bean(name = "batchKafkaListenerContainerFactoryForCreateEvent")
+    public ConcurrentKafkaListenerContainerFactory<Long, CreateTransactionRecord> batchKafkaListenerContainerFactoryForCreateEvent() {
+        ConcurrentKafkaListenerContainerFactory<Long, CreateTransactionRecord> factory = new ConcurrentKafkaListenerContainerFactory<>();
         ContainerProperties containerProperties = factory.getContainerProperties();
-        containerProperties.setIdleBetweenPolls(3000);
-        factory.setConsumerFactory(consumerFactory());
+        containerProperties.setIdleBetweenPolls(500);
+
+        factory.setConsumerFactory(consumerFactory(CreateTransactionRecord.class));
+        factory.setBatchListener(true);
         factory.setConcurrency(1);
         return factory;
+    }
+
+    @Bean(name = "batchKafkaListenerContainerFactoryForUpdateEvent")
+    public ConcurrentKafkaListenerContainerFactory<Long, UpdateTransactionRecord> batchKafkaListenerContainerFactoryForUpdateEvent() {
+        ConcurrentKafkaListenerContainerFactory<Long, UpdateTransactionRecord> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        ContainerProperties containerProperties = factory.getContainerProperties();
+        containerProperties.setIdleBetweenPolls(500);
+
+        factory.setBatchListener(true);
+        factory.setConsumerFactory(consumerFactory(UpdateTransactionRecord.class));
+        factory.setConcurrency(1);
+        return factory;
+    }
+
+    private <T extends EventRecord> ConsumerFactory<Long, T> consumerFactory(Class<T> eventRecordClass) {
+        Map<String, Object> properties = new HashMap<>();
+        properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaProperties.getBootstrapServers());
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, "query-service");
+        properties.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, 1000);
+        properties.put(ConsumerConfig.MAX_POLL_INTERVAL_MS_CONFIG, 200000);
+        properties.put(ConsumerConfig.RETRY_BACKOFF_MS_CONFIG, 1000);
+        properties.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, 9000);
+        properties.put(ConsumerConfig.HEARTBEAT_INTERVAL_MS_CONFIG, 3000);
+        properties.put(ConsumerConfig.FETCH_MIN_BYTES_CONFIG, 1024 * 1024);
+        properties.put(ConsumerConfig.FETCH_MAX_WAIT_MS_CONFIG, 1000);
+        return new DefaultKafkaConsumerFactory<>(properties, new LongDeserializer(), new JsonDeserializer<>(eventRecordClass, false));
     }
 
 }
